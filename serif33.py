691a1e3cf99025dd90c307a6f4540f505ce99b55
ovish <36478032+ovish@users.noreply.github.com>
2018-02-17 12:14:05 +0900

    [update]serif**.pyのコードの整理

--- serif33.py
+++ serif33.py
@@ -6,42 +6,24 @@ import sys
 import os
 import codecs
 
-os.chdir('/files/directory')
+os.chdir('/path/to/files/directory')
 
 # JpnとEngのリスト作成
 with open('TriJpnRaw.md', 'r') as f:
-    dataJpn = f.read()
-listJpn = dataJpn.split('\n')
-
+    listJpn = f.read().split('\n')
 with open('TriEngWork.md', 'r') as f:
-    dataEng = f.read()
-listEng = dataEng.split('\n')
+    listEng = f.read().split('\n')
 
-'''
-# 確認用pok
-print(listJpn)
-print(listEng)
-'''
-'''
-# リストから取り出してみたok
-num = 3
-nump = listJpn[num]
-print(nump)
-'''
 
 # {{セリフ3|番号|キャラ名（日本語）|キャラ名（英語）|セリフ（日）|セリフ（英）}}
-
-
 # リスト作る
-verseList = []
-for i, (lineJpni, lineEngi) in enumerate(zip(listJpn, listEng)):
-    iStr = str(i)
-    verse = '{{セリフ3' + '|' + iStr + '.' + '|nameJ|nameE|' +  lineJpni + '|' + lineEngi + '}}' + '\n'
-    verseList.append(verse)
+verseList = list(map(lambda
+    lineJpn, lineEng:
+    '{{セリフ3' + '|' + str(listJpn.index(lineJpn)) + '.' + '|nameJ|nameE|' + lineJpn + '|' + lineEng + '}}'
+    , listJpn, listEng
+    ))
 
 # 書き込むファイル開く、なければ作られる
-f = open('セリフ3.txt', 'w')
-for o in verseList:
-    f.write(str(o) + '\n')
-f.close()
+with open('セリフ3.txt', 'w') as f:
+    f.write('\n'.join(verseList))
 # おわり
